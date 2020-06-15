26c5a0f15799ebf0f70bf0322068968c9a92fab3
Derek Dodwell <55286286+Derek312@users.noreply.github.com>
2020-06-15 16:04:27 -0400

    Add files via upload

--- Module4ProjectJS/src/products.js
+++ Module4ProjectJS/src/products.js
@@ -17,7 +17,7 @@ function load(url, type) {
 }
 
 // Use json file to make a html page
-load('https://derek312.github.io/Module4ProjectJS/Module4ProjectJS/src/products.json', 'json').then(function(jsonObj) {
+load('https://derek312.github.io/Module4ProjectJS/products.json', 'json').then(function(jsonObj) {
     let products = jsonObj.strangeProducts;
     console.log("2");
     for(let i = 0; i < products.length; i++) {
@@ -31,19 +31,20 @@ load('https://derek312.github.io/Module4ProjectJS/Module4ProjectJS/src/products.
         //fill h2 text with the name from json file
         h2.textContent = products[i].name;
         //set image using the image names from the json and the github pages link
-        img.setAttribute('src', 'https://derek312.github.io/Module4ProjectJS/Module4ProjectJS/src/images/' + products[i].image);
+        img.setAttribute('src', 'https://derek312.github.io/Module4ProjectJS/images/' + products[i].image);
         img.setAttribute('alt', products[i].image);
         //set to price from the json
         p.textContent = "Price: $" + products[i].price;
         console.log("4");
         //create list elements using the json file details list
-        for(let h = 0; h < products[i].details.length; h++) {
-            let list = document.createElement('li');
-            list.textContent = products[i].details[h];
-            ul.appendChild(list);
+        for(let j = 0; j < products[i].details.length; j++) {
+            let li = document.createElement('li');
+            li.textContent = products[i].details[j];
+            ul.appendChild(li);
             console.log("5");
         }
-        // Add the elements to the html page
+
+        // Add elements to the page
         div.appendChild(h2);
         div.appendChild(img);
         div.appendChild(p);
